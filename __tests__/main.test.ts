bb4f0d8570f8636d40ac3dea771f0673d82629dc
Jupiter Bakakeu <jupiter.bakakeu@gmail.com>
2022-08-10 00:39:11 +0200

    prod dependencies

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,10 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {main, wait} from '../src/common'
 import {expect, test} from '@jest/globals'
+import { AlteryxSdk } from '@jupiterbak/ayx-node'
+import fs from 'fs'
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -17,13 +19,36 @@ test('wait 500 ms', async () => {
   expect(delta).toBeGreaterThan(450)
 })
 
+test('test main', async () => {
+  const url = 'http://ec2-3-66-136-169.eu-central-1.compute.amazonaws.com/webapi/';
+  const clientId = '8DA78CE09C0E5D1abf4927846637f9a02e196b8eff52b61f03246ad16ad2c81125ef4a80920db80';
+  const clientSecret = '1f675a0f8d2c572ddd02005a3396fe7e89706fe4a39e0d5f39cf9b6463aecec8';
+  const collectionName = '00_Data_Ingestion'
+
+  const sdk = new AlteryxSdk({
+    gateway: url,
+    clientId,
+    clientSecret
+  })
+  const wClient = sdk.GetWorkflowManagementClient()
+  const cClient = sdk.GetCollectionManagementClient()
+  const jClient = sdk.GetJobManagementClient()
+
+  const rslt = await main(cClient, wClient, jClient, collectionName)
+
+}, 500000)
+
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_AYX-SERVER-API-URL'] = 'http://ec2-3-66-136-169.eu-central-1.compute.amazonaws.com/webapi/'
+  process.env['INPUT_AYX-SERVER-CLIENT-ID'] = '8DA78CE09C0E5D1abf4927846637f9a02e196b8eff52b61f03246ad16ad2c81125ef4a80920db80'
+  process.env['INPUT_AYX-SERVER-CLIENT-SECRET'] = '1f675a0f8d2c572ddd02005a3396fe7e89706fe4a39e0d5f39cf9b6463aecec8'
+
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
-})
+}, 500000)
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,156 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {AYXTestResults, main, wait} from './common'
+import {AlteryxSdk} from '@jupiterbak/ayx-node'
+import fs from 'fs'
 
 async function run(): Promise<void> {
   try {
+    // read the inputs
     const ms: string = core.getInput('milliseconds')
+    const url: string = core.getInput('ayx-server-api-url')
+    const clientId: string = core.getInput('ayx-server-client-id')
+    const clientSecret: string = core.getInput('ayx-server-client-secret')
+    const collectionName: string = core.getInput('collection-to-test')
+
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+    // Instantiate the clients
+    // Instantiate the library
+    const sdk = new AlteryxSdk({
+      gateway: url,
+      clientId,
+      clientSecret
+    })
+    const wClient = sdk.GetWorkflowManagementClient()
+    const cClient = sdk.GetCollectionManagementClient()
+    const jClient = sdk.GetJobManagementClient()
+
+    const rslt: AYXTestResults = await main(
+      cClient,
+      wClient,
+      jClient,
+      collectionName
+    )
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+
+    // create test report
+    const tests = rslt.statuses.map((status, i) => {
+      return {
+        title: rslt.workflows[i].name,
+        fullTitle: rslt.workflows[i].name,
+        file: rslt.workflows[i].name,
+        duration: 0,
+        currentRetry: 0,
+        speed: 'fast',
+        err: {},
+        createDate: status.createDate,
+        disposition: status.disposition,
+        status: status.status,
+        name: rslt.workflows[i].name,
+        job: status.id,
+        runWithE2: status.runWithE2,
+        outputCount: status.outputs?.length
+      }
+    })
+
+    const passes = rslt.statuses
+      .filter(status => status.disposition === 'Success')
+      .map((status, i) => {
+        return {
+          title: rslt.workflows[i].name,
+          fullTitle: rslt.workflows[i].name,
+          file: rslt.workflows[i].name,
+          duration: 0,
+          currentRetry: 0,
+          speed: 'fast',
+          err: {},
+          createDate: status.createDate,
+          disposition: status.disposition,
+          status: status.status,
+          name: rslt.workflows[i].name,
+          job: status.id,
+          runWithE2: status.runWithE2,
+          outputCount: status.outputs?.length
+        }
+      })
+
+    const failures = rslt.statuses
+      .filter(status => status.disposition !== 'Success')
+      .map((status, i) => {
+        return {
+          title: rslt.workflows[i].name,
+          fullTitle: rslt.workflows[i].name,
+          file: rslt.workflows[i].name,
+          duration: 0,
+          currentRetry: 0,
+          speed: 'fast',
+          err: {},
+          createDate: status.createDate,
+          disposition: status.disposition,
+          status: status.status,
+          name: rslt.workflows[i].name,
+          job: status.id,
+          runWithE2: status.runWithE2,
+          outputCount: status.outputs?.length
+        }
+      })
+
+    const pending = rslt.statuses
+      .filter(
+        status =>
+          status.disposition === 'Running' || status.disposition === 'Queued'
+      )
+      .map((status, i) => {
+        return {
+          title: rslt.workflows[i].name,
+          fullTitle: rslt.workflows[i].name,
+          file: rslt.workflows[i].name,
+          duration: 0,
+          currentRetry: 0,
+          speed: 'fast',
+          err: {},
+          createDate: status.createDate,
+          disposition: status.disposition,
+          status: status.status,
+          name: rslt.workflows[i].name,
+          job: status.id,
+          runWithE2: status.runWithE2,
+          outputCount: status.outputs?.length
+        }
+      })
+
+    const test_result = {
+      stats: {
+        suites: 1,
+        tests: rslt.workflows.length,
+        passes: rslt.statuses.filter(status => status.disposition === 'Success')
+          .length,
+        pending: rslt.statuses.filter(
+          status =>
+            status.disposition === 'Running' || status.disposition === 'Queued'
+        ).length,
+        failures: rslt.statuses.filter(
+          status => status.disposition !== 'Success'
+        ).length,
+        start: rslt.statuses.reduce((p, c) => {
+          return new Date(c.createDate!) > new Date(p.createDate!) ? p : c
+        }).createDate,
+        end: rslt.statuses.reduce((p, c) => {
+          return new Date(c.createDate!) > new Date(p.createDate!) ? c : p
+        }).createDate,
+        duration: 0
+      },
+      tests,
+      pending,
+      failures,
+      passes
+    }
+
+    // Write results to file
+    fs.writeFileSync('results.json', JSON.stringify(test_result, null, 4))
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
